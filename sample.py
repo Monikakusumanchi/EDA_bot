fb564b17acad1408d7f106b2e2535d0d64bad232
Monika kusumanchi <kusumanchimonika1407@gmail.com>
2025-01-02 20:11:46 +0000

    added for all csvs

--- sample.py
+++
@@ -1,9 +0,0 @@
-
-from dotenv import load_dotenv
-import os
-# from .env import *  # relative import
-load_dotenv(dotenv_path='/workspace/EDA_bot/.env')
-
-
-# Access the OpenAI API key
-print(os.getenv("OPENAI_API_KEY"))
